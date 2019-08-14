73d04e637908d11ee6c3bf39559407c6ea8e1dc0
Martha Miao <mmiao@ualberta.ca>
2019-08-13 19:58:21 -0600

    Updated NEFtile2FeatureDataFrame.py

--- 4-ReadXMP2LabelsDataFrame.py
+++ 4-ReadXMP2LabelsDataFrame.py
@@ -84,11 +84,13 @@ def XMPFolder2LabelDataFrame(XMP_folder_path):
     
     LabelsDataFrame = pd.DataFrame(Edit_labels)
     LabelsDataFrame.columns = ["Exposure", "Contrast", "Highlights", "Shadows", "Temperature"]
-    Labels_df.index = Names
+    LabelsDataFrame.index = Names
 
     return LabelsDataFrame
 
 XMP_file_path = '/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/ED_DataExploration/ED_DataExploration_XMP'
+Df = XMPFolder2LabelDataFrame(XMP_file_path)
+Df
 Labels, Names = XMPFolder2LabelList(XMP_file_path)
 print(Labels)
 print(Names)
@@ -100,5 +102,91 @@ Labels_df = pd.DataFrame(Labels)
 Labels_df.columns=["Exposure", "Contrast", "Highlights", "Shadows", "Temperature"]
 Labels_df.index = Names
 
-Labels_df
+
+ED_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_XMP_IndoorPerson"
+JY_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/James & Yasmin Wedding/2017-08-06/JY_XMP_IndoorPerson"
+LM_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Lauren & Matt Wedding/2017-09-08/LM_XMP_IndoorPerson"
+LC_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Lorraine & Chad Wedding/2017-08-19/LC_XMP_IndoorPerson"
+
+
+ED_Label_IndoorPerson = XMPFolder2LabelDataFrame(ED_XMP_IndoorPerson) # There is a problem!
+
+JY_Label_IndoorPerson = XMPFolder2LabelDataFrame(JY_XMP_IndoorPerson)
+JY_Label_IndoorPerson.describe()
+
+LM_Label_IndoorPerson = XMPFolder2LabelDataFrame(LM_XMP_IndoorPerson)
+LM_Label_IndoorPerson.describe()
+
+LC_Label_IndoorPerson = XMPFolder2LabelDataFrame(LC_XMP_IndoorPerson)
+LC_Label_IndoorPerson.describe()
+
+
+
+import os
+### Test the XMP IndoorPerson folders
+# !!!!!! There is a problem with ED IndoorPerson folder, need to update
+XMP_folder_path = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_NEF"
+files = sorted(os.listdir(XMP_folder_path))
+Edit_labels = []
+Names = []
+
+for file_idx in range(len(files)):
+    file_name = files[file_idx]
+    XMP_file_path = os.path.join(XMP_folder_path,file_name)
+    parameter = XMP_parameter(XMP_file_path)
+    print(file_name)
+    print(parameter)
+    
+    Edit_labels.append(parameter)
+    Names.append(file_name)
+
+
+
+# JY XMP IndoorPerson folder has no problem
+XMP_folder_path = "/Users/user7/Downloads/HD6_12er/James & Yasmin Wedding/2017-08-06/JY_XMP_IndoorPerson"
+files = sorted(os.listdir(XMP_folder_path))
+Edit_labels = []
+Names = []
+
+for file_idx in range(len(files)):
+    file_name = files[file_idx]
+    XMP_file_path = os.path.join(XMP_folder_path,file_name)
+    parameter = XMP_parameter(XMP_file_path)
+    print(file_name)
+    print(parameter)
+    
+    Edit_labels.append(parameter)
+    Names.append(file_name)
+
+# LM XMP IndoorPerson folder has no problem
+LM_XMP_IndoorPerson_folder_path = "/Users/user7/Downloads/HD6_12er/Lauren & Matt Wedding/2017-09-08/LM_XMP_IndoorPerson"
+files = sorted(os.listdir(LM_XMP_IndoorPerson_folder_path))
+Edit_labels = []
+Names = []
+
+for file_idx in range(len(files)):
+    file_name = files[file_idx]
+    XMP_file_path = os.path.join(LM_XMP_IndoorPerson_folder_path,file_name)
+    parameter = XMP_parameter(XMP_file_path)
+    print(file_name)
+    print(parameter)
+    
+    Edit_labels.append(parameter)
+    Names.append(file_name)
+
+# LC XMP IndoorPerson folder has no problem
+LC_XMP_IndoorPerson_folder_path = "/Users/user7/Downloads/HD6_12er/Lorraine & Chad Wedding/2017-08-19/LC_XMP_IndoorPerson"
+files = sorted(os.listdir(LC_XMP_IndoorPerson_folder_path))
+Edit_labels = []
+Names = []
+
+for file_idx in range(len(files)):
+    file_name = files[file_idx]
+    XMP_file_path = os.path.join(LC_XMP_IndoorPerson_folder_path,file_name)
+    parameter = XMP_parameter(XMP_file_path)
+    print(file_name)
+    print(parameter)
+    
+    Edit_labels.append(parameter)
+    Names.append(file_name)
 
--- 4_NEFfiles2FeatureDataFrame.py
+++ 4_NEFfiles2FeatureDataFrame.py
@@ -1,32 +1,143 @@
 # Read through NEF folder 
 # Extract and normalize the distribution of R, G, B pixel values in bins of 16 as image feature
 # Save the features of all images in the folder to one Features dataframe
-
+# Two methods are created to read in the NEF file path and create Feature dataframe
 import os
 import rawpy
 import pandas as pd
 import numpy as np
+from pytictoc import TicToc
 
-def NEFImage2Features(NEF_folder_path)):
+
+#### Method 1: Combine the NEFFolder2FeatureList function and NEFfile2FeatureDF function
+def NEFFolder2FeatureList(NEF_folder_path):
+    import os 
+    # go through all XMP files in folder
+    files = sorted(os.listdir(NEF_folder_path))
+    Features = []
+
+    for file_idx in range(len(files)):
+        feature_list = []
+
+        file_name = files[file_idx]
+        NEF_file_path = os.path.join(NEF_folder_path,file_name)
+
+        rp_image = rawpy.imread(NEF_file_path)
+        rgb = rp_image.postprocess()
+
+        # red the R, G, B channels seperately and sort from 0 to 255
+        rgb_1 = np.sort(rgb[:,:,0].ravel())
+        rgb_2 = np.sort(rgb[:,:,1].ravel())
+        rgb_3 = np.sort(rgb[:,:,2].ravel())
+
+        dataset = pd.DataFrame({'Red': rgb_1, 'Green': rgb_2, 'Blue':rgb_3})
+
+        df_1 = dataset['Red'].value_counts(bins = 16, normalize = True).sort_index()
+        df_2 = dataset['Green'].value_counts(bins = 16, normalize = True).sort_index()
+        df_3 = dataset['Blue'].value_counts(bins = 16, normalize = True).sort_index()
+
+        feature_list.append(df_1.tolist())
+        feature_list.append(df_2.tolist())
+        feature_list.append(df_3.tolist())
+
+        Features.append(feature_list)
+
+    return Features
+
+def NEFfile2FeatureDF(NEF_folder_path):
+    FeatureList = NEFFolder2FeatureList(NEF_folder_path)
+    files = sorted(os.listdir(NEF_folder_path))
+
+    FeatureArray = np.array(FeatureList)
+    Features = pd.DataFrame()
+
+    for i in range(FeatureArray.shape[0]):
+        image_array = FeatureArray[i,:,:].ravel()
+        Features[files[i]] = image_array
+    
+    return Features.T
+
+t = TicToc() #create instance of class
+t.tic()
+NEF_folder_path = "/Users/user7/Desktop/WeddingImageProcessing/data/NEFs_test"
+Features_DF = NEFfile2FeatureDF(NEF_folder_path)
+Features_DF # no errow message!!!!
+t.toc() ## 114s
+
+
+
+##### Method 2: NEFFolder2Feature function
+def NEFFolder2Feature(NEF_folder_path):
+    import os 
+
+    Features = pd.DataFrame()
+
+    # go through all XMP files in folder
+    files = sorted(os.listdir(NEF_folder_path))
+    
+
+    for file_idx in range(len(files)):
+        feature_list = []
+
+        NEF_file_path = os.path.join(NEF_folder_path,files[file_idx])
+
+        rp_image = rawpy.imread(NEF_file_path)
+        rgb = rp_image.postprocess()
+
+        # red the R, G, B channels seperately and sort from 0 to 255
+        rgb_1 = np.sort(rgb[:,:,0].ravel())
+        rgb_2 = np.sort(rgb[:,:,1].ravel())
+        rgb_3 = np.sort(rgb[:,:,2].ravel())
+
+        dataset = pd.DataFrame({'Red': rgb_1, 'Green': rgb_2, 'Blue':rgb_3})
+
+        df_1 = dataset['Red'].value_counts(bins = 16, normalize = True).sort_index()
+        df_2 = dataset['Green'].value_counts(bins = 16, normalize = True).sort_index()
+        df_3 = dataset['Blue'].value_counts(bins = 16, normalize = True).sort_index()
+
+        feature_list.append(df_1.tolist())
+        feature_list.append(df_2.tolist())
+        feature_list.append(df_3.tolist())
+
+        feature_array = np.array(feature_list).ravel()
+
+        Features[files[file_idx]] = feature_array
+
+    return Features.T
+
+t = TicToc()
+t.tic()
+NEF_folder_path = "/Users/user7/Desktop/WeddingImageProcessing/data/NEFs_test"
+Features_DF2 = NEFFolder2Feature(NEF_folder_path)
+Features_DF2
+t.toc() ### 112s
+
+
+## to save dataframe to csv files
+def DataFrame2CSV(df, csv_folder_path, csv_file_name):
+    
+    csv_file_path = os.path.join(csv_folder_path, csv_file_name)
+    df.to_csv(csv_file_path)
+csv_folder_path = "/Users/user7/Desktop/WeddingImageProcessing/data"
+csv_file_name = "Features_DF.csv"
+
+DataFrame2CSV(Features_DF, csv_folder_path, csv_file_name)
+
+
+##### The following function does not work
+def NEFImage2Features(NEF_folder_path):
 
     # create a feature dataframe
     Features = pd.DataFrame()
-    
+
     # go through all NEF files in folder
     NEF_files = sorted(os.listdir(NEF_folder_path))
-    
+
     # Get the images and show the predicted classes
     for file_idx in range(len(NEF_files)):
-        # get the iamge name
-        image_name = NEF_files[file_idx]
 
         # get the NEF image file path
-        NEF_path = os.path.join(NEF_folder_path, NEF_files[file_idx])
-
-        # read the NEF image as RGB
-        import rawpy
-        import imageio
-    
+        NEF_path = os.path.join(NEF_folder_path, NEF_files[file_idx])    
         rp_image = rawpy.imread(NEF_path)
         rgb = rp_image.postprocess()
 
@@ -43,13 +154,20 @@ def NEFImage2Features(NEF_folder_path)):
         df_2 = dataset['Green'].value_counts(bins = 16, normalize = True).sort_index()
         df_3 = dataset['Blue'].value_counts(bins = 16, normalize = True).sort_index()
 
+
+        df_new = df_1.append(df_2)
+        df_new_new = df_new.append(df_3)
+
         # save R, G, B channel bins count into one column of a new temporary dataframe
-        temp_df = pd.DataFrame({"rgb": df_rgb})
-        Features[image_name] = temp_df
+        temp_df = pd.DataFrame({"rgb": df_new_new})
+        #print(temp_df)
+
+        Features[NEF_files[file_idx]] = temp_df['rgb']
 
     # transpose the Features dataframe to make sure each column represents the a feature
     Features = Features.T
 
     return Features
 
-    
+test_DF = pd.read_csv("/Users/user7/Desktop/WeddingImageProcessing/data/Features_DF.csv", header = 0)
+test_DF

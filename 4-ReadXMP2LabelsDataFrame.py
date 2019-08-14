14a7df734e64c06dcc2f2db897b2835dea8994c1
Martha Miao <mmiao@ualberta.ca>
2019-08-13 20:53:12 -0600

    updated ReadXMP2LabelsDataFrame.py works now

--- 4-ReadXMP2LabelsDataFrame.py
+++ 4-ReadXMP2LabelsDataFrame.py
@@ -1,4 +1,6 @@
 # Read in XMP Files and Extract the Exposure, Contrast, Highlight, Shadow, and Temperature parameters
+import os 
+
 def XMP_parameter(xmp_file_path):
     fd = open(xmp_file_path)
     d = fd.read()
@@ -53,7 +55,7 @@ def XMP_parameter(xmp_file_path):
 
 
 def XMPFolder2LabelList(XMP_folder_path):
-    import os 
+
     # go through all XMP files in folder
     files = sorted(os.listdir(XMP_folder_path))
     Edit_labels = []
@@ -68,8 +70,10 @@ def XMPFolder2LabelList(XMP_folder_path):
 
     return Edit_labels, Names
 
+
+    
 def XMPFolder2LabelDataFrame(XMP_folder_path):
-    import os 
+
     # go through all XMP files in folder
     files = sorted(os.listdir(XMP_folder_path))
     Edit_labels = []
@@ -88,30 +92,21 @@ def XMPFolder2LabelDataFrame(XMP_folder_path):
 
     return LabelsDataFrame
 
-XMP_file_path = '/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/ED_DataExploration/ED_DataExploration_XMP'
-Df = XMPFolder2LabelDataFrame(XMP_file_path)
-Df
-Labels, Names = XMPFolder2LabelList(XMP_file_path)
-print(Labels)
-print(Names)
-print(len(Labels))
-Labels = np.array(Labels)
-Labels.shape
-
-Labels_df = pd.DataFrame(Labels)
-Labels_df.columns=["Exposure", "Contrast", "Highlights", "Shadows", "Temperature"]
-Labels_df.index = Names
-
 
 ED_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_XMP_IndoorPerson"
 JY_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/James & Yasmin Wedding/2017-08-06/JY_XMP_IndoorPerson"
 LM_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Lauren & Matt Wedding/2017-09-08/LM_XMP_IndoorPerson"
 LC_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Lorraine & Chad Wedding/2017-08-19/LC_XMP_IndoorPerson"
 
+# ED folder need to remove the .DS_Store files first
+os.remove("/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_XMP_IndoorPerson/.DS_Store")
 
+ED_XMP_IndoorPerson = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/ED_XMP_IndoorPerson1"
 ED_Label_IndoorPerson = XMPFolder2LabelDataFrame(ED_XMP_IndoorPerson) # There is a problem!
+ED_Label_IndoorPerson
 
 JY_Label_IndoorPerson = XMPFolder2LabelDataFrame(JY_XMP_IndoorPerson)
+JY_Label_IndoorPerson
 JY_Label_IndoorPerson.describe()
 
 LM_Label_IndoorPerson = XMPFolder2LabelDataFrame(LM_XMP_IndoorPerson)
@@ -120,78 +115,4 @@ LM_Label_IndoorPerson.describe()
 LC_Label_IndoorPerson = XMPFolder2LabelDataFrame(LC_XMP_IndoorPerson)
 LC_Label_IndoorPerson.describe()
 
-
-
-import os
-### Test the XMP IndoorPerson folders
-# Export the XMP files for all the ED wedding NEF files, and test it. it works.
-#XMP_folder_path = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_NEF_new"
-
-# Now try to work with IndoorPerson XMP files
-
-XMP_folder_path = "/Users/user7/Downloads/HD6_12er/Erin & Dan Wedding/2017-09-23/ED_NEF_new"
-files = sorted(os.listdir(XMP_folder_path))
-Edit_labels = []
-Names = []
-
-for file_idx in range(len(files)):
-    file_name = files[file_idx]
-    XMP_file_path = os.path.join(XMP_folder_path,file_name)
-    parameter = XMP_parameter(XMP_file_path)
-    print(file_name)
-    print(parameter)
-    
-    Edit_labels.append(parameter)
-    Names.append(file_name)
-
-len(Edit_labels)
-
-
-# JY XMP IndoorPerson folder has no problem
-XMP_folder_path = "/Users/user7/Downloads/HD6_12er/James & Yasmin Wedding/2017-08-06/JY_XMP_IndoorPerson"
-files = sorted(os.listdir(XMP_folder_path))
-Edit_labels = []
-Names = []
-
-for file_idx in range(len(files)):
-    file_name = files[file_idx]
-    XMP_file_path = os.path.join(XMP_folder_path,file_name)
-    parameter = XMP_parameter(XMP_file_path)
-    print(file_name)
-    print(parameter)
-    
-    Edit_labels.append(parameter)
-    Names.append(file_name)
-
-# LM XMP IndoorPerson folder has no problem
-LM_XMP_IndoorPerson_folder_path = "/Users/user7/Downloads/HD6_12er/Lauren & Matt Wedding/2017-09-08/LM_XMP_IndoorPerson"
-files = sorted(os.listdir(LM_XMP_IndoorPerson_folder_path))
-Edit_labels = []
-Names = []
-
-for file_idx in range(len(files)):
-    file_name = files[file_idx]
-    XMP_file_path = os.path.join(LM_XMP_IndoorPerson_folder_path,file_name)
-    parameter = XMP_parameter(XMP_file_path)
-    print(file_name)
-    print(parameter)
-    
-    Edit_labels.append(parameter)
-    Names.append(file_name)
-
-# LC XMP IndoorPerson folder has no problem
-LC_XMP_IndoorPerson_folder_path = "/Users/user7/Downloads/HD6_12er/Lorraine & Chad Wedding/2017-08-19/LC_XMP_IndoorPerson"
-files = sorted(os.listdir(LC_XMP_IndoorPerson_folder_path))
-Edit_labels = []
-Names = []
-
-for file_idx in range(len(files)):
-    file_name = files[file_idx]
-    XMP_file_path = os.path.join(LC_XMP_IndoorPerson_folder_path,file_name)
-    parameter = XMP_parameter(XMP_file_path)
-    print(file_name)
-    print(parameter)
-    
-    Edit_labels.append(parameter)
-    Names.append(file_name)
 
